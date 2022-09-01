73ef88bf34389e9b5c255d90e11711f45b23de19
Premod1 <premodsuraweera1@gmail.com>
2022-09-01 16:36:58 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <SignupForm/>
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue';  
+export default {
+    name:'App',
+    components: {
+      SignupForm
+    }
+}
+</script>
+
+<style>
+ 
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
